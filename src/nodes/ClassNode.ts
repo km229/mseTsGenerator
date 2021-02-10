bc4d62c333b701b4be5e543feca792e4ecb2a6e2
Kévin MORICEAU <kevin.moriceau@viacesi.fr>
2021-02-10 11:09:24 +0100

    Démarrage du traitement des classes

--- src/nodes/ClassNode.ts
+++ src/nodes/ClassNode.ts
@@ -1,18 +1,62 @@
 
 import * as src from "./index"
 import {ClassDeclaration} from "ts-morph";
+import {Element} from "../model/Element";
+import {MSEDocument} from "./index";
 
-export class ClassNode extends src.Node {
+export class ClassNode extends src.Node<ClassDeclaration> {
 
-    constructor(node: ClassDeclaration, ctx: src.MSEDocument) {
-        super(node, ctx)
+    constructor(node: ClassDeclaration, ctx: MSEDocument) {
+        super(ctx, node, new Element(ctx.getNextId, "Class", [
+            ['name', node.getName()]
+        ]))
     }
 
     explore(): void {
-    }
 
-    toMSE(): string {
-        return "";
+        //Add fileAnchor for this class
+        let fileAnchor = new Element(this._ctx.getNextId, "IndexedFileAnchor", [
+            ["element", `(ref: ${this._element.id})`],
+            ["startPos", String(this._node.getPos())],
+            ["endPos", String(this._node.getEnd())],
+            ["fileName", `'${this._node.getSourceFile().getFilePath()}`],
+        ])
+        this._element.addAttribute("sourceAnchor", `(ref: ${fileAnchor.id})`)
+        this._elementList.push(fileAnchor)
+
+        // Add modifiers
+        if(this._node.getModifiers().length > 0){
+            let modifiers = ""
+            this._node.getModifiers().forEach(modifier => {
+                modifiers += `'${modifier.getText()}'`
+            })
+            this._element.addAttribute('modifiers', modifiers)
+        }
+        // let extend= this._node.getExtends();
+        // let implement = this._node.getImplements()
+        // console.log(extend)
+        // console.log(implement)
+        // console.log(this._node.getDerivedClasses())
+        // this._node.getInstanceMembers().forEach(attribut => {
+        //     console.log(attribut)
+        // })
+        this._node.forEachChild(child => {
+            let currentNode
+            switch (child.getKind()){
+                // case ts.SyntaxKind.HeritageClause:
+                //     currentNode = new src.ClassNode(child as ClassDeclaration, this._ctx)
+                //     this._nodeList.push(currentNode)
+                //     break;
+                // case ts.SyntaxKind.PublicKeyword:
+                //     currentNode = new src.ClassNode(child as ClassDeclaration, this._ctx)
+                //     this._nodeList.push(currentNode)
+                //     break;
+            }
+            if(currentNode != undefined){
+                currentNode.explore()
+            }
+            currentNode=undefined
+        })
     }
 
 }
--- src/nodes/FileNode.ts
+++ src/nodes/FileNode.ts
@@ -1,25 +1,34 @@
 
 import * as src from "./index"
-import {SourceFile} from "ts-morph"
+import {ClassDeclaration, SourceFile, ts} from "ts-morph"
+import {Element} from "../model/Element";
 
-export class FileNode extends src.Node {
+export class FileNode extends src.Node<SourceFile> {
 
-    constructor(node: SourceFile, ctx: src.MSEDocument) {
-        super(node, ctx)
-    }
-
-    public checkNodes(){
-
-    }
-
-    public toMSE(): string {
-        let result = src.def.OPEN_TOKEN+"\n"
-        result += src.def.CLOSE_TOKEN;
-        return result;
+    constructor(ctx: src.MSEDocument, node: SourceFile) {
+        super(ctx, node, new Element(ctx.getNextId, "FileAnchor", [
+            ["fileName", node.getFilePath()],
+            ["startLine", String(node.getStartLineNumber())],
+            ["endLine", String(node.getEndLineNumber())],
+        ]))
     }
 
     explore(): void {
+        this._node.forEachChild(node => {
+            switch (node.getKind()){
+                case ts.SyntaxKind.ClassDeclaration:
+                    this._nodeList.push(new src.ClassNode(node as ClassDeclaration, this._ctx))
+                    break;
+                case ts.SyntaxKind.InterfaceDeclaration:
+                    break;
+                case ts.SyntaxKind.ModuleDeclaration:
+                    break;
+                case ts.SyntaxKind.ImportDeclaration:
+                    break;
+                case ts.SyntaxKind.VariableStatement:
+                    break;
+            }
+        })
     }
-
 
 }
